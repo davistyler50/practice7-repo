ca8646ae63ed43d30b6d6291b5f5b84125b708d2
Tyler Davis <tyl2181858@maricopa.edu>
2019-06-22 20:55:44 -0700

    message

--- practice7.cpp
+++ practice7.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-  string thisStr = "this is a short string\n";
+  string thisStr = "this is a loooooooooooooooooooooooooong string\n";
   cout << thisStr << endl;
 
   return 0;
